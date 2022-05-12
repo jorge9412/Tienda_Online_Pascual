2f0eb2a048e414ad6958ca549f539e1e2e67bdce
jorge9412 <coke9412@hotmail.com>
2022-05-12 18:51:15 -0500

    agregando models contacto y migrate

--- contacto/models.py
+++ contacto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Contacto(models.Model):
+    id_contacto = models.CharField(primary_key = True, max_length=50 , default="")
+    asunto = models.CharField(max_length=50)
+    correo = models.EmailField()
+    mensaje = models.CharField(max_length=200)
+
