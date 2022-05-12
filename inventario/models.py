0e9082ad0d79a2f5c2e29fa2054f1e2f83061012
jorge9412 <coke9412@hotmail.com>
2022-05-12 15:51:45 -0500

    organizando errores

---
+++ inventario/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Productos(models.Model):
+    id_producto = models.CharField(primary_key = True, max_length=50 , default="")
+    name = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
+    cost = models.IntegerField(default=0)
+    cantidad_stock = models.IntegerField(default=0)
+    description = models.TextField(max_length=30)
