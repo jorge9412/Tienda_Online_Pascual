7381a16f9877e736667aecb6e08a9d642b8e63be
jorge9412 <coke9412@hotmail.com>
2022-05-21 17:14:33 -0500

    creo models venta

--- ventas/models.py
+++ ventas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from sqlalchemy import false
 
-# Create your models here.
+from inventario.models import Productos
+
+class Venta(models.Model):
+    id_venta = models.AutoField(primary_key = True)
+    nombres = models.TextField(max_length=30)
+    tipo_doc = models.CharField(max_length=30)
+    num_doc = models.IntegerField(default=0)
+    correo = models.EmailField()
+    direccion = models.CharField(max_length=30)
+    depto = models.CharField(max_length=30)
+    ciudad = models.CharField(max_length=30)
+    telefono = models.IntegerField(max_length=30)
+    id_producto = models.OneToOneField(Productos,on_delete=models.CASCADE,
+                                        blank=False,null=False)
